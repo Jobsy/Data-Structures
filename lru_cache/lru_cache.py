c70158d346ce67d600385d3e22be04686d6471e6
Oluwajoba <oluwajobabello@gmail.com>
2020-02-26 04:49:05 +0100

    incomplete / test not passing

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+import sys
+sys.path.append('../doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.current_num_of_node = 0
+        self.key_value_entries = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +25,21 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # for k in key:
+        #     print(k.values, "the key, and value")
+        # value = self.storage[key]
+        # while self.current_num_of_node < self.limit:
+        if key in self.storage:
+            key_value = self.storage[key]
+            # self.current_num_of_node += 1
+            self.key_value_entries.move_to_front(key_value)
+            return key_value.value
+        else:
+            return None
+
+        # pass
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +51,6 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
         pass
